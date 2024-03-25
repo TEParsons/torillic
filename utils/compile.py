10eb1fb08b62100ac7ac670e59df2fa5c6203709
TEParsons <todd.e.parsons@gmail.com>
2024-03-25 20:40:33 +0000

    refac: Replace base Torillic folders with stub files and a script to populate

---
+++ utils/compile.py
@@ -0,0 +1,25 @@
+"""
+Copy the files from base Torillic into the various locations where there's a stub file for it.
+"""
+
+import shutil
+from pathlib import Path
+
+# ref to this folder
+folder = Path(__file__).parent
+# ref to root folder
+root = folder.parent
+# ref to base folder
+base = root / "base"
+# find all .stub files...
+for stub in root.glob("**/*.stub"):
+    # convert the stub to a folder
+    target = stub.parent / stub.stem
+    # delete the stub file
+    # stub.unlink()
+    # copy the base files to the new folder
+    shutil.copytree(base, target)
+    # log
+    print(
+        f"Copied {base} to {target}."
+    )
